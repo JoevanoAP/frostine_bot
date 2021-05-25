8899fd316965f1bfa2425464eef5e93c7e535dc8
JoevanoAP <usvaino@gmail.com>
2021-05-25 21:01:56 +0700

    hey

--- index.js
+++ index.js
@@ -11,23 +11,25 @@ command(frostine, 'ping', message => {
     message.reply('Heyy!! Im Here!! What you want to do?')
 })
 
-command(frostine, 'help', message => {
-    const help = new Discord.MessageEmbed()
-    .setTitle('Frostine Bot Plugins Commands')
-    .setDescription('Prefix (+)')
-    .addField('help commands', 'to see all commands', true)
-    .addField('help moderator', 'to see all moderator commands', true)
-    .setColor('69fff0');
-    message.channel.send(help);
-})
-
-command(frostine,'help commands', message => {
-    const help_commands = new Discord.MessageEmbed()
-    .setTitle('Commands Plugin')
-    .setDescription('This command allowed for all members')
-    .addField('ping', 'to check bot online or not')
-    .setColor('69fff0')
-    message.channel.send(help_commands)
-})
+if(command === 'help'){
+    command(frostine, 'help', message => {
+        const help = new Discord.MessageEmbed()
+        .setTitle('Frostine Bot Plugins Commands')
+        .setDescription('Prefix (+)')
+        .addField('help commands', 'to see all commands', true)
+        .addField('help moderator', 'to see all moderator commands', true)
+        .setColor('69fff0');
+        message.channel.send(help);
+    })
+} else {
+    command(frostine,'help commands', message => {
+        const help_commands = new Discord.MessageEmbed()
+        .setTitle('Commands Plugin')
+        .setDescription('This command allowed for all members')
+        .addField('ping', 'to check bot online or not')
+        .setColor('69fff0')
+        message.channel.send(help_commands)
+    })
+}
 
 frostine.login(config.token)
