9d0f6a2977a57a26c0080667c192dc6c9227ceca
JoevanoAP <usvaino@gmail.com>
2021-05-25 20:52:20 +0700

    command

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ command(frostine, 'ping', message => {
     message.reply('Heyy!! Im Here!! What you want to do?')
 })
 
+const help = command(frostine, 'help', message => {
+    message.channel.send = new Discord.MessageEmbed()
+    .setTitle('Frostine Bot Plugins Commands')
+    .setDescription('Prefix (+)')
+    .addField('help commands', 'to see all commands', true)
+    .addField('help moderator', 'to see all moderator commands', true)
+    .setColor('69fff0');
+})
+
 frostine.login(config.token)
