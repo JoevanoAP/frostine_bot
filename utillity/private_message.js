8a8cac064df10c2a0edb92252d1ec74d4dfdaa62
joevano_ <67526262+JoevanoAP@users.noreply.github.com>
2021-06-20 09:47:57 +0700

    Delete utility folder
    change to handlers

--- utillity/private_message.js
+++
@@ -1,7 +0,0 @@
-module.exports = (frostine, triggerText, replyText) => {
-    frostine.on('message', message => {
-        if(message.content.toLowerCase() === triggerText.toLowerCase()){
-            message.author.send(replyText)
-        }
-    })
-}
